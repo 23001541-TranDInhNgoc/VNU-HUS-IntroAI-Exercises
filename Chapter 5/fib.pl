d6416de05d9a0e2ede6028ea2703aced79355818
NgocTD <23001541@hus.edu.vn>
2025-10-20 07:42:42 +0700

    Update fib.pl

--- Chapter 5/fib.pl
+++ Chapter 5/fib.pl
@@ -1,1 +1,14 @@
 % Create a solution for Exercise 5.8(a)
+
+The naive definition of the Fibonacci function in Prolog:
+
+    fib(0,1).
+    fib(1,1).
+    fib(N,R) :-
+        N1 is N - 1, fib(N1, R1),
+        N2 is N - 2, fib(N2, R2),
+        R is R1 + R2.
+
+Explanation:
+Each call to fib/2 generates two recursive subcalls.
+The recursion tree grows exponentially because many subresults are recomputed.
